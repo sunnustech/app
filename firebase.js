b53efe1509c94682c9bf007ee8d5613097b17fe2
nguyenvukhang <brew4k@gmail.com>
2022-01-04 16:06:06 +0800

    exposed firebase.js, hid secrets.json

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import secrets from './secrets.json'
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = secrets.firebase
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+export const auth = getAuth(firebaseApp);
+
+// Please test login functionality with the following admin account:
+// Email: sunnus@gmail.com
+// Password: test1234
