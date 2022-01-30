458af9ed1f987b38846d462da69f791255e3a642
nguyenvukhang <brew4k@gmail.com>
2022-01-30 22:03:09 +0800

    completed csv parsing

--- src/lib/csv-to-json/index.mjs
+++ src/lib/csv-to-json/index.mjs
@@ -11,7 +11,6 @@ function getCSV(filename) {
 
 function handlePartcipants() {
   const data = getCSV('participants.csv')
-  const outputFile = 'participants.json'
 
   /* use first row as headers */
   const headers = data.shift()
@@ -85,5 +84,81 @@ function handleTSS() {
   return { first_32, schedule }
 }
 
+function handleSOAR() {
+  const data = getCSV('SOAR.csv')
+
+  function parseLocations(data) {
+    const locations = data.map((row) => row.slice(0, 6))
+
+    const headers = locations.shift()
+
+    /* some parsing helpers */
+    const parseKey = (key) => {
+      return key.replace('?', '').replace('/', '')
+    }
+    const parseValue = (value) => {
+      switch (value) {
+        case 'yes':
+          return true
+        case 'no':
+          return false
+        default:
+          return value
+      }
+    }
+
+    /* turn each row into an object, taking keys from the header row */
+    const zipped = locations
+      .map((row) =>
+        headers.reduce(
+          (obj, key, index) => ({
+            ...obj,
+            [parseKey(key)]: parseValue(row[index]),
+          }),
+          {}
+        )
+      )
+      .filter((e) => e.title != '')
+    return zipped
+  }
+
+  function parseSchedules(data, locations) {
+    const idx = data[0].reduce((res, element, idx) => {
+      element === 'time' && res.push(idx)
+      return res
+    }, [])
+    const output = []
+    idx.forEach((i) => {
+      const title = data[0][i - 1]
+      const locationList = data.map((row) => row.slice(i, i + 2))
+      const headers = locationList.shift()
+      const schedule = locationList
+        .map((row) =>
+          headers.reduce(
+            (obj, key, index) => ({
+              ...obj,
+              [key]: row[index],
+            }),
+            {}
+          )
+        )
+        .filter((e) => e.time != '')
+      const found = locations.find((e) => e.title === title)
+      output.push({ ...found, schedule })
+    })
+    return output
+  }
+
+  const locations = parseLocations(data)
+  const output = parseSchedules(data, locations)
+  return output
+}
+
 const participants = handlePartcipants()
-const { first_32, schedule } = handleTSS()
+const TSS = handleTSS()
+const SOAR = handleSOAR()
+
+/* write to json files */
+fs.writeFileSync('participants.json', JSON.stringify(participants))
+fs.writeFileSync('TSS.json', JSON.stringify(TSS))
+fs.writeFileSync('SOAR.json', JSON.stringify(SOAR))
--- src/lib/csv-to-json/readme.md
+++ src/lib/csv-to-json/readme.md
@@ -5,6 +5,7 @@ Usage:
 ```
 
 required files:
+obtain by downloading from [](https://docs.google.com/spreadsheets/d/1CroeOg-aoLfvnB8xT-UqOQRl-ISHaiIA4wAn4qbPmxs)
 
 - participants.csv
 - TSS.csv
