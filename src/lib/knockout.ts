4d19a8c59261292d7e8f57ab837c357465307aca
nguyenvukhang <brew4k@gmail.com>
2022-02-05 17:26:20 +0800

    factored out the backend fo match handling

---
+++ src/lib/knockout.ts
@@ -0,0 +1,80 @@
+import TSS from '@/data/schema/TSS'
+import push from '@/data/push'
+import { MatchRequest, Round } from './knockout.d'
+
+const delimiter = () => {
+  const date = new Date().toString()
+  const line = '='.repeat(date.length)
+  console.log(`\n\n\n${line}\n${date}\n${line}\n`)
+}
+
+function resetTSS() {
+  push({ collection: 'TSS', data: TSS, merge: false })
+}
+
+/*
+ * have tabs to toggle which knockout table to view
+ * dodgeball/volleyball/tchoukball/frisbee...
+ */
+
+/* BACKEND FUNCTIONS */
+
+/*
+ * takes in
+ *  1. event type (dodgeball/volleyball/tchoukball/frisbee...)
+ *  2. match id
+ *  3. match winner
+ * writes the outcome to database
+ *
+ */
+
+const roundOrder: Array<Round> = [
+  'round_of_32',
+  'round_of_16',
+  'quarterfinals',
+  'semifinals',
+  'finals',
+]
+
+function handleMatch({ sport, round, matchNumber, winner }: MatchRequest) {
+  delimiter()
+
+  if (round === 'finals') {
+    // handle champions
+  }
+
+  const nextRound: Round = roundOrder[roundOrder.indexOf(round) + 1]
+  console.log('nextRound', nextRound)
+  const nextMatchNumber = Math.floor(matchNumber / 2)
+  const nextMatch = TSS[sport][nextRound][nextMatchNumber]
+
+  // get that exact match
+  const match = TSS[sport][round][matchNumber]
+
+  // create a packet containing only that match's data + outcome
+  const packet = {
+    [sport]: {
+      // update the outcome of that match
+      [round]: {
+        [matchNumber]: {
+          ...match,
+          winner,
+        },
+      },
+      // apend the schedule for next match
+      [nextRound]: {
+        [nextMatchNumber]: {
+          ...nextMatch,
+          [matchNumber % 2 === 0 ? 'A' : 'B']: match[winner],
+        },
+      },
+    },
+  }
+
+  // update the database
+  push({ collection: 'TSS', data: packet })
+
+  // push that team into the next round
+}
+
+export { resetTSS, handleMatch }
--- src/screens/KnockoutTable.tsx
+++ src/screens/KnockoutTable.tsx
@@ -1,124 +1,14 @@
 import { KeyboardAvoidingView, Text, View } from 'react-native'
 
 /* sunnus components */
-import { Button, ButtonGreen, ButtonRed } from '@/components/Buttons'
+import { Button, ButtonGreen } from '@/components/Buttons'
 import styles from '@/styles/main'
-import TSS from '@/data/schema/TSS'
-import push from '@/data/push'
-
-// just a simple console delimiter
-const delimiter = () => {
-  const date = new Date().toString()
-  const line = '='.repeat(date.length)
-  console.log(`\n\n\n${line}\n${date}\n${line}\n`)
-}
-
-function resetTSS() {
-  push({ collection: 'TSS', data: TSS, merge: false })
-}
-
-/*
- * have tabs to toggle which knockout table to view
- * dodgeball/volleyball/tchoukball/frisbee...
- */
-
-/* BACKEND FUNCTIONS */
-
-/*
- * takes in
- *  1. event type (dodgeball/volleyball/tchoukball/frisbee...)
- *  2. match id
- *  3. match winner
- * writes the outcome to database
- *
- */
-type MatchRequest = {
-  sport: 'dodgeball' | 'frisbee' | 'volleyball' | 'tchoukball'
-  matchNumber: number
-  winner: 'A' | 'B'
-  round: Round
-}
-
-type Round =
-  | 'round_of_32'
-  | 'round_of_16'
-  | 'quarterfinals'
-  | 'semifinals'
-  | 'finals'
-
-const roundOrder: Array<Round> = [
-  'round_of_32',
-  'round_of_16',
-  'quarterfinals',
-  'semifinals',
-  'finals',
-]
-
-function handleMatch({ sport, round, matchNumber, winner }: MatchRequest) {
-  delimiter()
-
-  if (round === 'finals') {
-    // handle champions
-  }
-
-  const nextRound: Round = roundOrder[roundOrder.indexOf(round) + 1]
-  console.log('nextRound', nextRound)
-  const nextMatchNumber = Math.floor(matchNumber / 2)
-  const nextMatch = TSS[sport][nextRound][nextMatchNumber]
-
-  // get that exact match
-  const match = TSS[sport][round][matchNumber]
-
-  // create a packet containing only that match's data + outcome
-  const packet = {
-    [sport]: {
-      [round]: {
-        [matchNumber]: {
-          ...match,
-          winner,
-        },
-      },
-      [nextRound]: {
-        [nextMatchNumber]: {
-          ...nextMatch,
-          [matchNumber % 2 === 0 ? 'A' : 'B']: match[winner],
-        },
-      },
-    },
-  }
-
-  // apend the schedule for next match
-
-  // update the database
-  push({ collection: 'TSS', data: packet })
-
-  // push that team into the next round
-}
-
-function debug() {
-  delimiter()
-
-  // get match details
-  const round = 'round_of_32'
-  const matchNumber = 0
-  const match = TSS.volleyball[round][matchNumber]
-
-  // set winner
-  const winner = 'A'
-  match.winner = match[winner]
-
-  console.log('round:', round)
-  console.log('match:', matchNumber)
-  console.log(match)
-  console.log(TSS.volleyball[round][matchNumber])
-
-  // update the database
-  push({ collection: 'TSS', data: TSS })
-}
+import { resetTSS, handleMatch } from '@/lib/knockout'
+import { MatchRequest } from '@/lib/knockout.d'
 
 const KnockoutTable = () => {
   // to be interactively keyed in eventually
-  const matchData = {
+  const matchData: MatchRequest = {
     sport: 'volleyball',
     round: 'round_of_16',
     matchNumber: 5,
@@ -135,7 +25,6 @@ const KnockoutTable = () => {
           volleyball
         </Text>
         <Button onPress={() => handleMatch(matchData)}>Handle Match End</Button>
-        <ButtonRed onPress={debug}>Debug</ButtonRed>
       </View>
     </KeyboardAvoidingView>
   )
