b1b6533c678758a896b48b91bd38fc0f79662aa3
nguyenvukhang <brew4k@gmail.com>
2022-02-05 12:33:30 +0800

    cleaned up knockout table page

--- src/screens/KnockoutTable.tsx
+++ src/screens/KnockoutTable.tsx
@@ -1,36 +1,24 @@
-import { KeyboardAvoidingView, Text, Button, View } from 'react-native'
+import { KeyboardAvoidingView, Text, View } from 'react-native'
 
 /* sunnus components */
-import { auth } from '@/sunnus/firebase'
-import { notifications as styles } from '@/styles/main'
-import { sendPushNotification, notificationInit } from '@/lib/notifications'
+import { auth, db } from '@/sunnus/firebase'
+import { Button, ButtonGreen } from '@/components/Buttons'
+import styles from '@/styles/main'
+import typedTSS from '@/data/schema/TSS'
+import push from '@/data/push'
+
+function resetTSS() {
+  push({ collection: 'TSS', data: typedTSS })
+}
 
 const KnockoutTable = () => {
-  /*
-   * have tabs to toggle which knockout table to view
-   * dodgeball/volleyball/tchoukball/frisbee...
-   */
-
-  /* BACKEND FUNCTIONS */
-
-  /*
-   * takes in
-   *  1. event type (dodgeball/volleyball/tchoukball/frisbee...)
-   *  2. match id
-   *  3. match winner
-   * writes the outcome to database
-   *
-   */
-  function handleEndOfMatch() {}
-
-  /*
-   *
-   */
   return (
-    <KeyboardAvoidingView
-      style={styles.container}
-      behavior="padding"
-    ></KeyboardAvoidingView>
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
+      <Text>The one place for knockout table development and debugging</Text>
+      <View style={styles.buttonContainer}>
+        <ButtonGreen onPress={() => resetTSS()}>Reset TSS Data</ButtonGreen>
+      </View>
+    </KeyboardAvoidingView>
   )
 }
 
