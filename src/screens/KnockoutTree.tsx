3c5fac57235a2d9b75e75984241853ae0413b94a
nguyenvukhang <brew4k@gmail.com>
2022-02-26 11:11:15 +0800

    first semblance of a table

--- src/screens/KnockoutTree.tsx
+++ src/screens/KnockoutTree.tsx
@@ -5,18 +5,50 @@ import RNPickerSelect from 'react-native-picker-select'
 /* sunnus components */
 import { Button, ButtonGreen, ButtonRed } from '@/components/Buttons'
 import styles from '@/styles/main'
-import { delimiter, getKnockoutTable } from '@/lib/knockout'
+import { delimiter, getKnockoutTree } from '@/lib/knockout'
 import TSS from '@/data/schema/TSS'
 import { Sport } from '@/data/schema/TSS.d'
 import { MatchRequest, Round } from '@/types/TSS.d'
 import { useState } from 'react'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import tw from 'twrnc'
 // }}}
 
-const KnockoutTable = () => {
+const MatchNode = ({ text, gap }: { text: string; gap: number }) => {
+  return (
+    <TouchableOpacity
+      style={tw`flex flex-row justify-center items-center h-10 w-36 bg-blue-200 p-2 border mx-3 my-${gap}`}
+    >
+      <Text>{`A vs. B ${text}`}</Text>
+    </TouchableOpacity>
+  )
+}
+
+const ColumnMatchNodes = ({
+  nodes,
+  gap = 0,
+}: {
+  nodes: number
+  gap?: number
+}) => {
+  return (
+    <View>
+      {[...Array(nodes).keys()].map((e, i) => (
+        <MatchNode text={e.toString()} key={i} gap={gap} />
+      ))}
+    </View>
+  )
+}
+
+const Column = ({ children }: { children: any }) => {
+  return <View style={tw`flex flex-col justify-center`}>{children}</View>
+}
+
+const KnockoutTree = () => {
   const [sport, setSport] = useState<Sport>('volleyball')
 
-  function debugKnockoutTable({ sport }: { sport: Sport }) {
-    getKnockoutTable({ sport }).then((data) => {
+  function debugKnockoutTree({ sport }: { sport: Sport }) {
+    getKnockoutTree({ sport }).then((data: any) => {
       delimiter()
       console.log(sport, data)
       delimiter()
@@ -27,12 +59,38 @@ const KnockoutTable = () => {
     <KeyboardAvoidingView style={styles.container}>
       <Text>The one place for knockout tree development and debugging</Text>
       <View style={styles.buttonContainer}>
-        <ButtonGreen onPress={() => debugKnockoutTable({ sport })}>
+        <ButtonGreen onPress={() => debugKnockoutTree({ sport })}>
           Debug Table
         </ButtonGreen>
       </View>
+      <ScrollView
+        style={tw`bg-green-200 w-full`}
+        horizontal={true}
+        directionalLockEnabled={false}
+        showsHorizontalScrollIndicator={false}
+      >
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <View style={tw`flex flex-row`}>
+            <Column>
+              <ColumnMatchNodes nodes={16} gap={2} />
+            </Column>
+            <Column>
+              <ColumnMatchNodes nodes={8} gap={9} />
+            </Column>
+            <Column>
+              <ColumnMatchNodes nodes={4} gap={23} />
+            </Column>
+            <Column>
+              <ColumnMatchNodes nodes={2} gap={51} />
+            </Column>
+            <Column>
+              <MatchNode text="finals" gap={0} />
+            </Column>
+          </View>
+        </ScrollView>
+      </ScrollView>
     </KeyboardAvoidingView>
   )
 }
 
-export default KnockoutTable
+export default KnockoutTree
