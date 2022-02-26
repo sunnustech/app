09b2b69329dde27f568e16fb6e87d0f31da5dc36
nguyenvukhang <brew4k@gmail.com>
2022-02-26 23:13:34 +0800

    removed tw

--- src/components/Knockout.tsx
+++ src/components/Knockout.tsx
@@ -5,14 +5,10 @@ import {
   View,
   StyleSheet,
 } from 'react-native'
-import tw from 'twrnc'
 
 const MatchNode = ({ text, styleProps }: { text: string; styleProps: any }) => {
   return (
-    <TouchableOpacity
-      style={styles.matchNode(styleProps)}
-      // style={tw`flex flex-row justify-center items-center h-10 w-36 p-2 border mx-3 my-${gap}`}
-    >
+    <TouchableOpacity style={styles.matchNode(styleProps)}>
       <Text>{`A vs. B ${text}`}</Text>
     </TouchableOpacity>
   )
@@ -28,7 +24,7 @@ const MatchColumn = ({
   color: string
 }) => {
   return (
-    <View style={tw`flex flex-col justify-center ${color}`}>
+    <View style={styles.matchColumn({ color })}>
       <View>
         {[...Array(nodes).keys()].map((e, i) => (
           <MatchNode text={e.toString()} key={i} styleProps={styleProps} />
@@ -39,19 +35,35 @@ const MatchColumn = ({
 }
 
 const Columns = () => {
-  const styleProps = {
-    height: 30,
-    gap: 4,
+  const gaps = []
+  const height = 10
+  const gap = 4
+  const columns = 5
+  for (let i = 0; i < columns - 1; i++) {
+    const nodes = 2 ** (columns - i - 1)
+    const n = 2 ** i
+    const thisGap = (n * gap + (n - 1) * height) / 1
+    gaps.push({ gap: thisGap, nodes })
   }
+  gaps.push({ gap: 0, nodes: 1 })
+  console.log('got here', gaps, height)
+
   return (
     <>
-      <MatchColumn color="bg-red-100" nodes={16} styleProps={styleProps} />
-      <MatchColumn color="bg-orange-100" nodes={8} styleProps={styleProps} />
-      <MatchColumn color="bg-yellow-100" nodes={4} styleProps={styleProps} />
-      <MatchColumn color="bg-green-100" nodes={2} styleProps={styleProps} />
-      <MatchColumn color="bg-blue-100" nodes={1} styleProps={styleProps} />
+      {gaps.map((e) => (
+        <MatchColumn
+          color="#fee2e2"
+          nodes={e.nodes}
+          styleProps={{ gap: e.gap, height }}
+        />
+      ))}
     </>
   )
+  // <MatchColumn color="#fee2e2" nodes={16} styleProps={styleProps}
+  // <MatchColumn color="#ffedd5" nodes={8} styleProps={styleProps}
+  // <MatchColumn color="#fef9c3" nodes={4} styleProps={styleProps}
+  // <MatchColumn color="#dcfce7" nodes={2} styleProps={styleProps}
+  // <MatchColumn color="#dbeafe" nodes={1} styleProps={styleProps}
 }
 
 const Knockout = () => {
@@ -88,11 +100,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     height: height,
     width: 100,
-    padding: 2,
+    padding: 0,
     marginHorizontal: 8,
     marginVertical: gap,
     borderStyle: 'solid',
-    borderWidth: 1,
+    borderWidth: 10,
+  }),
+  matchColumn: ({ color }: { color: string }) => ({
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    backgroundColor: color,
   }),
 })
 
