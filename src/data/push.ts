f10885606f94a73ecd351107b57e6f9fc5d820c8
nguyenvukhang <brew4k@gmail.com>
2022-03-19 10:48:23 +0800

    wrote docs for push.ts

--- src/data/push.ts
+++ src/data/push.ts
@@ -1,19 +1,42 @@
-/* firebase */
 import { setDoc, doc as fsdoc, writeBatch } from '@firebase/firestore'
-
-/* sunnus components */
 import { db } from '@/sunnus/firebase'
 
 type FirestoreRequest = {
   collection: string
   docs: {
-    [doc: string]: any
+    [key: string]: {
+      [key: string]: any
+    }
   }
+  // merge: true  => update parts that changed only
+  // merge: false => overwrite database entirely
+  // default is true
   merge?: boolean
 }
 
+/*
+ * example FirestoreRequest:
+ * ----------------------------------------
+ * const foo = {
+ *   collection: 'TSS',
+ *   docs: {
+ *     'dodgeball': {
+ *       'finals': [...],
+ *       'semifinals': [...],
+ *     }
+ *   },
+ *   merge: true
+ * }
+ * ----------------------------------------
+ * To be Firestore-friendly:
+ *   1. `docs` has to be an Object.
+ *   2. `dodgeball`-level keys must be strings
+ *   3. `finals`-level keys must be strings
+ *   4. any data below `finals` can be of any type
+ */
+
 const push = async ({ collection, docs, merge = true }: FirestoreRequest) => {
-  /* create the collection if doesn't yet exist, and push the date */
+  /* create the collection if doesn't exist yet, and push the date */
   await setDoc(fsdoc(db, collection, '.init'), {
     date: new Date().toString(),
   })
@@ -30,8 +53,8 @@ const push = async ({ collection, docs, merge = true }: FirestoreRequest) => {
   /* execute all writes in the queue with .commit */
   try {
     await batch.commit()
-  } catch (e) {
-    console.error('Error adding document: ', e)
+  } catch (err) {
+    console.error('Error adding document: ', err)
   }
 }
 
