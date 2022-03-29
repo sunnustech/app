a2497766c3790e118af7d4c8c77a82cd137c1b72
nguyenvukhang <brew4k@gmail.com>
2022-03-29 21:21:55 +0800

    relatively stable build state

---
+++ docs/BUILD.md
@@ -0,0 +1,13 @@
+To build, make sure you have npm package `eas` installed, then run
+
+`eas build -p ios`
+
+or simply
+
+`yarn eas`
+
+monitor progress on expo.dev
+
+download the resultant file
+
+upload with Transporter
--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp, getApp, getApps } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
-import secrets from './secrets.json'
+import secrets from './secrets'
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
