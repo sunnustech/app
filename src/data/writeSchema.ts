a1913907bfc2d2a0047b1e730f4feaca1928bad2
nguyenvukhang <brew4k@gmail.com>
2022-04-03 21:41:56 +0800

    removed csv-json parser

--- src/data/writeSchema.ts
+++ src/data/writeSchema.ts
@@ -1,9 +1,5 @@
 import push from '@/data/push'
 
-// import jsonParticipants from '@/data/csv-json/participants.json'
-// import jsonTSS from '@/data/csv-json/TSS.json'
-// import jsonSOAR from '@/data/csv-json/SOAR.json'
-
 import typedParticipants from '@/data/schema/participants'
 import typedTSS from '@/data/schema/TSS'
 import typedSOAR from '@/data/schema/SOAR'
