35ac1818f4aa7c02712f020acc666dca4afc9edf
nguyenvukhang <brew4k@gmail.com>
2022-04-06 21:39:00 +0800

    improve TSS listener

--- src/navigations/TSSNavigator.tsx
+++ src/navigations/TSSNavigator.tsx
@@ -1,14 +1,58 @@
 import TSSScreen from '@/screens/TSS'
 import TSSKnockoutTable from '@/screens/TSS/KnockoutTable'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { useFocusEffect } from '@react-navigation/native'
+import { useCallback, useEffect, useState } from 'react'
+import { onSnapshot, doc } from 'firebase/firestore'
+import { Rounds, Sport } from '@/types/TSS'
+import { db } from '@/sunnus/firebase'
 
 const TSSTabs = createBottomTabNavigator()
 
-const TSSNavigator = () => (
-  <TSSTabs.Navigator>
-    <TSSTabs.Screen name="TSSScreen" component={TSSScreen} />
-    <TSSTabs.Screen name="KnockoutTable" component={TSSKnockoutTable} />
-  </TSSTabs.Navigator>
-)
+const TSSNavigator = () => {
+  const sportState = useState<Sport>('volleyball')
+  const [sport, setSport] = sportState
+  const [data, setData] = useState<Rounds>()
+  const [TSSNavActive, setTSSNavActive] = useState<boolean>(false)
+
+  const KnockoutTableWrapper = () => {
+    return <TSSKnockoutTable sportState={sportState} data={data} />
+  }
+
+  useEffect(() => {
+    if (TSSNavActive) {
+      const unsubscribeFirebase = onSnapshot(doc(db, 'TSS', sport), (doc) => {
+        const liveData = doc.data()
+        if (liveData) {
+          console.log(`received firebase updates for ${sport} at`, new Date())
+          // console.log(liveData)
+        }
+      })
+      return () => {
+        /* detach firebase listener on unmount */
+        console.log('detach firebase listener on TSS navigator')
+        unsubscribeFirebase()
+      }
+    }
+  }, [TSSNavActive, sport])
+
+  useFocusEffect(
+    useCallback(() => {
+      console.log('focused on TSS navigator')
+      setTSSNavActive(true)
+      return () => {
+        console.log('unfocused TSS navigator')
+        setTSSNavActive(false)
+      }
+    }, [])
+  )
+
+  return (
+    <TSSTabs.Navigator>
+      <TSSTabs.Screen name="TSSScreen" component={TSSScreen} />
+      <TSSTabs.Screen name="KnockoutTable" component={KnockoutTableWrapper} />
+    </TSSTabs.Navigator>
+  )
+}
 
 export default TSSNavigator
--- src/screens/TSS/KnockoutTable.tsx
+++ src/screens/TSS/KnockoutTable.tsx
@@ -12,40 +12,14 @@ import { Sport } from '@/types/TSS'
 import { db } from '@/sunnus/firebase'
 import { TeamProps } from '@/types/participants'
 
-const KnockoutTable = () => {
+const KnockoutTable = ({ sportState }: any) => {
   const navigation = useNavigation<TSSPage<'TSSKnockoutTable'>>()
-
-  const [sport, setSport] = useState<Sport>('volleyball')
-
-  useEffect(() => {
-    const unsubscribeFirebase = onSnapshot(doc(db, 'TSS', sport), (doc) => {
-      const liveData = doc.data()
-      if (liveData) {
-        console.log('received firebase TSS kt updates at', new Date())
-        const updatedTeamData: TeamProps = {
-          SOARTimerEvents: liveData.SOARTimerEvents,
-          SOARStart: liveData.SOARStart,
-          teamName: liveData.teamName,
-          SOAR: liveData.SOAR,
-          members: liveData.members,
-          registeredEvents: liveData.registeredEvents,
-          SOARPausedAt: liveData.SOARPausedAt,
-          SOARStationsCompleted: liveData.SOARStationsCompleted,
-          SOARStationsRemaining: liveData.SOARStationsRemaining,
-        }
-      }
-    })
-    return () => {
-      /* detach firebase listener on unmount */
-      console.log('detach firebase listener on TSS knockout')
-      unsubscribeFirebase()
-    }
-  }, [])
+  const [sport, setSport] = sportState
 
   return (
     <KeyboardAvoidingView style={styles.container} behavior="padding">
       <Text>Welcome to the TSS Knockout Table!</Text>
-      <Text>Choose sport</Text>
+      <Text>Sport: {sport}</Text>
     </KeyboardAvoidingView>
   )
 }
