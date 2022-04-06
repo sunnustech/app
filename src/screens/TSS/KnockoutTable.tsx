bb5847145cee4b0a85db3347c40c65285ef86d5d
nguyenvukhang <brew4k@gmail.com>
2022-04-06 21:42:24 +0800

    improve sport picker

--- src/screens/TSS/KnockoutTable.tsx
+++ src/screens/TSS/KnockoutTable.tsx
@@ -1,6 +1,5 @@
 import { KeyboardAvoidingView, Text } from 'react-native'
-import { useEffect, useState } from 'react'
-import { onSnapshot, doc } from 'firebase/firestore'
+import RNPickerSelect from 'react-native-picker-select'
 
 /* navigation */
 import { TSSPage } from '@/types/navigation'
@@ -8,16 +7,26 @@ import { useNavigation } from '@react-navigation/native'
 
 /* sunnus components */
 import { TSS as styles } from '@/styles/fresh'
+import { useState } from 'react'
 import { Sport } from '@/types/TSS'
-import { db } from '@/sunnus/firebase'
-import { TeamProps } from '@/types/participants'
 
 const KnockoutTable = ({ sportState }: any) => {
   const navigation = useNavigation<TSSPage<'TSSKnockoutTable'>>()
   const [sport, setSport] = sportState
+  const [tempSport, setTempSport] = useState<Sport>()
 
   return (
     <KeyboardAvoidingView style={styles.container} behavior="padding">
+      <RNPickerSelect
+        onValueChange={(value) => setTempSport(value)}
+        onDonePress={() => setSport(tempSport)}
+        items={[
+          { label: 'dodgeball', value: 'dodgeball', key: 0 },
+          { label: 'frisbee', value: 'frisbee', key: 1 },
+          { label: 'tchoukball', value: 'tchoukball', key: 2 },
+          { label: 'volleyball', value: 'volleyball', key: 3 },
+        ]}
+      />
       <Text>Welcome to the TSS Knockout Table!</Text>
       <Text>Sport: {sport}</Text>
     </KeyboardAvoidingView>
