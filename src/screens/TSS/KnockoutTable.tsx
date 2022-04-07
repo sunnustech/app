5d799a695a73024fc91df5bfd52bde8bf7eda1bd
nguyenvukhang <brew4k@gmail.com>
2022-04-07 17:08:14 +0800

    moved stuff out

--- src/screens/TSS/KnockoutTable.tsx
+++ src/screens/TSS/KnockoutTable.tsx
@@ -8,90 +8,10 @@ import { useNavigation } from '@react-navigation/native'
 /* sunnus components */
 import { knockout as styles } from '@/styles/fresh'
 import { useState } from 'react'
-import { Match, Round, Sport } from '@/types/TSS'
+import { Match, Sport } from '@/types/TSS'
 import { sportList } from '@/data/constants'
-import PagerView from 'react-native-pager-view'
-import { filledRounds } from '@/data/schema/TSS'
 import { roundList } from '@/lib/knockout'
-
-const VSpacer = ({ h }: { h: number }) => <View style={{ height: h }} />
-
-const MatchNode = ({ match }: { match: Match }) => {
-  // TODO: incorporate scores into Match classes
-  const scores = [1, 0]
-
-  const Row = ({ team, score }: { team: string; score: number }) => {
-    return (
-      <View style={styles.matchNodeRow}>
-        <View style={styles.matchNodeTeam}>
-          <Text style={styles.matchNodeTeamText} numberOfLines={1}>
-            {team}
-          </Text>
-        </View>
-        <View style={styles.matchNodeScore}>
-          <Text style={styles.matchNodeScoreText} numberOfLines={1}>
-            {score}
-          </Text>
-        </View>
-      </View>
-    )
-  }
-
-  return (
-    <View style={styles.matchNodeContainer}>
-      <Row team={match.A} score={scores[0]} />
-      <VSpacer h={8} />
-      <Row team={match.B} score={scores[1]} />
-    </View>
-  )
-}
-
-const RoundOf32 = () => {
-  const data = filledRounds.round_of_32
-  return (
-    <PagerView
-      style={styles.pagerView}
-      initialPage={0}
-      showPageIndicator={true}
-      pageMargin={0}
-    >
-      {Object.keys(data).map((e: string, i) => {
-        const key = parseInt(e)
-        return (
-          <View
-            key={i}
-            style={{ flexDirection: 'row', justifyContent: 'center' }}
-          >
-            <MatchNode match={data[key]} />
-          </View>
-        )
-      })}
-    </PagerView>
-  )
-}
-
-const PagerRound = ({ round, key }: { round: Round; key: number }) => {
-  const data = filledRounds[round]
-  return (
-    <PagerView
-      key={key}
-      style={styles.pagerView}
-      initialPage={0}
-    >
-      {Object.keys(data).map((e: string, i) => {
-        const key = parseInt(e)
-        return (
-          <View
-            key={i}
-            style={{ flexDirection: 'row', justifyContent: 'center' }}
-          >
-            <MatchNode match={data[key]} />
-          </View>
-        )
-      })}
-    </PagerView>
-  )
-}
+import PagerRound from '@/components/TSS/Round'
 
 const All = () => {
   return (
