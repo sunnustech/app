0fcb9d5ea5c19a121f6572ca306f10b2c9d0cb41
nguyenvukhang <brew4k@gmail.com>
2022-04-07 17:10:58 +0800

    gonna try refs

--- src/screens/TSS/KnockoutTable.tsx
+++ src/screens/TSS/KnockoutTable.tsx
@@ -8,16 +8,17 @@ import { useNavigation } from '@react-navigation/native'
 /* sunnus components */
 import { knockout as styles } from '@/styles/fresh'
 import { useState } from 'react'
-import { Match, Sport } from '@/types/TSS'
+import { Sport } from '@/types/TSS'
 import { sportList } from '@/data/constants'
 import { roundList } from '@/lib/knockout'
 import PagerRound from '@/components/TSS/Round'
 
 const All = () => {
+  const reversedRoundList = roundList.reverse()
   return (
     <>
-      {roundList.reverse().map((round, i) => {
-        return <PagerRound round={round} key={i} />
+      {reversedRoundList.map((round, idx) => {
+        return <PagerRound round={round} key={idx} />
       })}
     </>
   )
@@ -27,6 +28,7 @@ const KnockoutTable = ({ sportState }: any) => {
   const navigation = useNavigation<TSSPage<'TSSKnockoutTable'>>()
   const [sport, setSport] = sportState
   const [tempSport, setTempSport] = useState<Sport>(sport)
+  const [page32, setPage32] = useState(0)
 
   /* currently when you change the sport using the picker,
    * the picker seems to jump back to showing
@@ -35,11 +37,6 @@ const KnockoutTable = ({ sportState }: any) => {
    *
    * may not need to fix if method of choosing sport changes later
    */
-  const testMatch: Match = {
-    A: 'Independent_Decorators',
-    B: 'Gentle_Sweaters',
-    winner: 'U',
-  }
   return (
     <KeyboardAvoidingView style={styles.container} behavior="padding">
       <RNPickerSelect
