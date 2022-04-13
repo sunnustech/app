1460d39ce85eb646b2b431db5b372a968f38e03b
nguyenvukhang <brew4k@gmail.com>
2022-04-13 18:08:51 +0800

    add notification indicator

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -1,14 +1,24 @@
 import { View, TouchableOpacity } from 'react-native'
 import colors from '@/styles/colors'
-import { AntDesign } from '@expo/vector-icons'
+import { MaterialCommunityIcons as MCI } from '@expo/vector-icons'
 import Sunnus from '@/components/svgs/Sunnus'
 import { Overlap } from '@/components/Views'
 import { home as styles } from '@/styles/fresh'
 
+const Circle = () => {
+  return <View style={styles.circle} />
+}
+
+const HiddenCircle = () => {
+  return <View style={[styles.circle, styles.transparent]} />
+}
+
 const Heart = ({ alert }: { alert: boolean }) => {
   return (
     <TouchableOpacity style={styles.headerButton}>
-      <AntDesign name="hearto" size={20} color={colors.homeFg} />
+      {alert ? <HiddenCircle /> : null}
+      <MCI name={'heart-outline'} size={24} color={colors.homeFg} />
+      {alert ? <Circle /> : null}
     </TouchableOpacity>
   )
 }
@@ -26,7 +36,7 @@ const Header = () => {
       <Overlap>
         <View style={styles.iconsContainer}>
           <View style={{ flex: 1 }} />
-          <Heart alert={false} />
+          <Heart alert={true} />
         </View>
       </Overlap>
     </View>
