74c60dba7aac20b1d7469a4935351b04122e2f76
nguyenvukhang <brew4k@gmail.com>
2022-05-14 11:26:52 +0800

    ignore lib warnings

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 /* navigation */
 import { NavigationContainer } from '@react-navigation/native'
+import { LogBox } from 'react-native'
 
 /* sunnus contexts */
 import SunNUS from '@/navigations/index'
+LogBox.ignoreLogs(['Setting a timer', 'AsyncStorage has been'])
 
 const App = () => {
   return (
