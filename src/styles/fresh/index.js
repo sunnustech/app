25495e0ba2667bda5c0d274b922d7e4048cd3488
nguyenvukhang <brew4k@gmail.com>
2022-05-15 21:56:49 +0800

    retire home.js

--- src/styles/fresh/index.js
+++ src/styles/fresh/index.js
@@ -4,7 +4,6 @@ import TSS from './TSS'
 import WSS from './WSS'
 import back from './back'
 import buttons from './buttons'
-import home from './home'
 import knockout from './knockout'
 import login from './login'
 import map from './map'
@@ -22,7 +21,6 @@ export {
   WSS,
   back,
   buttons,
-  home,
   knockout,
   login,
   map,
