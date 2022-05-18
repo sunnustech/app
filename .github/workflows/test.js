1118a66b39338efb42a5e75441dccd4fb45a98c7
nguyenvukhang <brew4k@gmail.com>
2022-05-18 13:55:21 +0800

    try sending

---
+++ .github/workflows/test.js
@@ -0,0 +1,61 @@
+const yes = {
+  ok: true,
+  result: [
+    {
+      update_id: 502073508,
+      my_chat_member: {
+        chat: {
+          id: -646035026,
+          title: 'Build Notifications',
+          type: 'group',
+          all_members_are_administrators: true,
+        },
+        from: {
+          id: 257481278,
+          is_bot: false,
+          first_name: 'khang',
+          username: 'nguyenvukhang',
+        },
+        date: 1652853224,
+        old_chat_member: {
+          user: {
+            id: 5283818611,
+            is_bot: true,
+            first_name: 'Expo Build',
+            username: 'expo_build_bot',
+          },
+          status: 'left',
+        },
+        new_chat_member: {
+          user: {
+            id: 5283818611,
+            is_bot: true,
+            first_name: 'Expo Build',
+            username: 'expo_build_bot',
+          },
+          status: 'member',
+        },
+      },
+    },
+    {
+      update_id: 502073509,
+      message: {
+        message_id: 3,
+        from: {
+          id: 257481278,
+          is_bot: false,
+          first_name: 'khang',
+          username: 'nguyenvukhang',
+        },
+        chat: {
+          id: -646035026,
+          title: 'Build Notifications',
+          type: 'group',
+          all_members_are_administrators: true,
+        },
+        date: 1652853224,
+        group_chat_created: true,
+      },
+    },
+  ],
+}
