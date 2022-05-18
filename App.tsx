904917f3799b5e5559e4e969169296105d860d7e
nguyenvukhang <brew4k@gmail.com>
2022-05-18 10:26:09 +0800

    remove LogBox

--- App.tsx
+++ App.tsx
@@ -1,12 +1,10 @@
 /* navigation */
 import { NavigationContainer } from '@react-navigation/native'
-import { LogBox } from 'react-native'
 
 /* sunnus contexts */
 import SunNUS from '@/navigations/index'
 
 const App = () => {
-  LogBox.ignoreLogs(['Setting a timer', 'AsyncStorage', 'ViewPropTypes will be'])
   return (
     <NavigationContainer>
       <SunNUS />
