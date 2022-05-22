a8fe1f518d5b902e5eea90ca75d6ba313ddfb6fe
nguyenvukhang <brew4k@gmail.com>
2022-05-22 12:43:07 +0800

    cleanup

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -2,8 +2,8 @@ import React, { createContext, useEffect, useState } from 'react'
 import { auth, db } from '@/sunnus/firebase'
 import { notificationInit } from '@/lib/notifications'
 import { doc, updateDoc } from 'firebase/firestore'
-import { UseState } from '../types/SOAR'
-import { User } from '../classes/user'
+import { UseState } from '@/types/SOAR'
+import { User } from '@/classes/user'
 
 type UserContextProps = {
   userState: UseState<User>
@@ -13,7 +13,7 @@ const UserContext = createContext<UserContextProps>({
   userState: [User.empty, () => {}],
 })
 
-async function handlePushTokens(token: string) {
+function handlePushTokens(token: string) {
   if (auth.currentUser === null) return
   updateDoc(doc(db, 'users', auth.currentUser.uid), {
     expoPushToken: token,
@@ -24,23 +24,11 @@ async function handlePushTokens(token: string) {
 
 const UserProvider = (props: React.PropsWithChildren<{}>) => {
   const userState = useState(User.empty)
-
   const token = notificationInit().expoPushToken
-
   useEffect(() => {
-    if (token !== '') {
-      handlePushTokens(token)
-    }
+    if (token !== '') handlePushTokens(token)
   }, [token])
-
-  return (
-    <UserContext.Provider
-      value={{
-        userState,
-      }}
-      {...props}
-    />
-  )
+  return <UserContext.Provider value={{ userState }} {...props} />
 }
 
 export { UserContext, UserProvider }
