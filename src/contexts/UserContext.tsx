30abc4adc9dd1dedb2dee7202389352393b7261e
nguyenvukhang <brew4k@gmail.com>
2022-05-22 12:41:54 +0800

    remove all other user context props

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -15,14 +15,6 @@ import { UseState } from '../types/SOAR'
 import { User } from '../classes/user'
 
 type UserContextProps = {
-  userId: string
-  setUserId: Dispatch<SetStateAction<string>>
-  teamName: string
-  setTeamName: Dispatch<SetStateAction<string>>
-  schedule: object
-  setSchedule: Dispatch<SetStateAction<object>>
-  teamData: TeamProps
-  setTeamData: Dispatch<SetStateAction<TeamProps>>
   userState: UseState<User>
 }
 
@@ -34,47 +26,8 @@ const teamDataInit = newSunNUSTeam({
 })
 
 const UserContext = createContext<UserContextProps>({
-  userId: '',
-  setUserId: () => '',
-  teamName: '',
-  setTeamName: () => '',
-  schedule: {},
-  setSchedule: () => {},
-  teamData: teamDataInit,
-  setTeamData: () => {},
   userState: [User.empty, () => {}],
 })
-
-const rehydrateUserData = async ({
-  setTeamName,
-  setUserId,
-  setSchedule,
-  setTeamData,
-}: any) => {
-  if (auth.currentUser) {
-    /*
-     * theoretically, this if-block will always be reached because
-     * rehydrateUserData will only be called if auth is not null
-     */
-    const userData = (
-      await pullDoc({
-        collection: 'users',
-        doc: auth.currentUser.uid,
-      })
-    ).data
-    setUserId(userData.loginId)
-    setTeamName(userData.teamName)
-
-    const teamData: any = (
-      await pullDoc({
-        collection: 'teams',
-        doc: userData.teamName,
-      })
-    ).data
-
-    setTeamData(teamData)
-  }
-}
 
 async function handlePushTokens(token: string) {
   if (auth.currentUser === null) {
@@ -88,10 +41,6 @@ async function handlePushTokens(token: string) {
 }
 
 const UserProvider = (props: React.PropsWithChildren<{}>) => {
-  const [userId, setUserId] = useState('')
-  const [teamName, setTeamName] = useState('')
-  const [schedule, setSchedule] = useState({})
-  const [teamData, setTeamData] = useState<TeamProps>(teamDataInit)
   const userState = useState(User.empty)
 
   const token = notificationInit().expoPushToken
@@ -105,14 +54,6 @@ const UserProvider = (props: React.PropsWithChildren<{}>) => {
   return (
     <UserContext.Provider
       value={{
-        userId,
-        setUserId,
-        teamName,
-        setTeamName,
-        schedule,
-        setSchedule,
-        teamData,
-        setTeamData,
         userState,
       }}
       {...props}
